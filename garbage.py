7cadbf167483570f1528a2c8bd319d6ecf5297ed
Parksoonil <100554960+Parksoonil@users.noreply.github.com>
2022-03-11 14:46:19 +0900

    Create garbage.py

---
+++ garbage.py
@@ -0,0 +1,13 @@
+class Test:
+    def __init__(self, name):
+        self.name = name
+        print("{} - 생성되었습니다.".format(self.name))
+    def __del__(self):
+        print("{} - 파괴되었습니다.".format(self.name))
+Test("A")
+Test("B")
+Test("C")
+print()
+a = Test("a")
+b = Test("b")
+c = Test("c")
