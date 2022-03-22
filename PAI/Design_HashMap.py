170499221cc35b62b093440a3a1ecf92120a42e4
Parksoonil <100554960+Parksoonil@users.noreply.github.com>
2022-03-22 17:09:09 +0900

    Create Design_HashMap.py

---
+++ PAI/Design_HashMap.py
@@ -0,0 +1,52 @@
+import collections
+
+
+class ListNode:
+    def __init__(self, key = None, val = None):
+        self.key = key
+        self.val = val
+        self.next = None
+class MyhashMap:
+    def __init__(self):
+        self.size = 1000
+        self.table = collections.defaultdict(ListNode)
+    def put(self, key: int, value: int) -> None:
+        index = key % self.size
+        if self.table[index].val is None:
+            self.table[index] = ListNode(key, value)
+            return
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                p.val = value
+                return
+            if p.next is None:
+                break
+            p = p.next
+        p.next = ListNode(key, value)
+    def get(self, key: int) -> int:
+        index = key % self.size
+        if self.table[index].val is None:
+            return -1
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                return p.val
+            p = p.next
+        return -1
+    def remove(self, key: int) -> None:
+        index = key % self.size
+        if self.table[index].val is None:
+            return
+        p = self.table[index]
+        if p.key == key:
+            self.table[index] = ListNode() if p.next is None else p.next
+            return
+        prev = p
+        while p:
+            if p.key == key: 
+                prev.next = p.next
+                return
+            prev, p = p, p.next
+
+
