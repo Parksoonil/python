16788981caee441f7b91604e2b16735f0e291fcb
Parksoonil <100554960+Parksoonil@users.noreply.github.com>
2022-03-26 15:27:31 +0900

    Create Maximum_Depth.py

---
+++ PAI/Maximum_Depth.py
@@ -0,0 +1,42 @@
+import collections
+
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Tree:
+    def __init__(self):
+        self.root = None
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        queue = collections.deque([root])
+        depth = 0
+
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                cur_root = queue.popleft()
+                if cur_root.left:
+                    queue.append(cur_root.left)
+                if cur_root.right:
+                    queue.append(cur_root.right)
+        return depth
+
+n1 = TreeNode(3)
+n2 = TreeNode(9)
+n3 = TreeNode(20)
+n4 = TreeNode(None)
+n5 = TreeNode(None)
+n6 = TreeNode(15)
+n7 = TreeNode(7)
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+n3.left = n6
+n3.right = n7
+s = Solution()
+print(s.maxDepth(n1))
