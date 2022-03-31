ea769c1f64d0efe3f80d04ad8725284d942a3a81
Parksoonil <100554960+Parksoonil@users.noreply.github.com>
2022-03-31 16:15:59 +0900

    Create K_Closest_Points.py

---
+++ PAI/K_Closest_Points.py
@@ -0,0 +1,17 @@
+import heapq
+from pip import List
+
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        heap = []
+        for(x, y) in points:
+            dist = x ** 2 + y ** 2
+            heapq.heappush(heap, (dist, x, y))
+        result = []
+        for _ in range(K):
+            (dist, x, y) = heapq.heappop(heap)
+            result.append((x, y))
+        return result
+s = Solution()
+print(s.kClosest([[1, 3], [-2, 2]], 1))
+print(s.kClosest([[3, 3], [5, -1], [-2, 4]],2))
